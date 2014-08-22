1e919fd2f5c03836971d3759154af717b7dce80f
realslimkarthik <karthikhariharan13@gmail.com>
2014-08-22 00:33:05 -0500

    Refactoring code

--- commentScrape.py
+++ commentScrape.py
@@ -2,11 +2,52 @@ import urllib2
 from bs4 import BeautifulSoup, SoupStrainer
 import nltk
 
+# class Player:
+#     def __init__(self, team, player, rating, position):
+#         self.t = team
+#         self.p = player
+#         self.r = rating
+#         self.pos = position
+
+def getTeam(link):
+    response = urllib2.urlopen(link)
+    html_doc = response.read()
+
+    soup = BeautifulSoup(html_doc)
+
+    listData = soup.find_all('ul', {'class' : 'sq-lst'})
+
+    hLineup = soup.find('a', {'href': '#teamlineup-homeTeam'})
+    aLineup = soup.find('a', {'href': '#teamlineup-awayTeam'})
+    home = str(hLineup.contents[0].strip())
+    away = str(aLineup.contents[0].strip())
+    teams[home] = []
+    teams[away] = []
+
+    listH = listData[1].find_all('li')
+    listHS = listData[2].find_all('li')
+    listH += listHS
+
+    for i in listH:
+        sp = i.find_all("span")
+        pl = sp[1].contents
+        teams[home].append(str(pl[0].strip()))
+
+    listA = listData[4].find_all('li')
+    listAS = listData[5].find_all('li')
+    listA += listHS
+
+    for i in listA:
+        sp = i.find_all("span")
+        pl = sp[1].contents
+        teams[away].append(str(pl[0].strip()))
+
+
 class Events:
-	def __init__(self, time, highlight, data):
-		self.t = time
-		self.hl = highlight
-		self.d = data
+    def __init__(self, time, highlight, data):
+        self.t = time
+        self.hl = highlight
+        self.d = data
 
 link = "http://www1.skysports.com/football/live/match/279816/commentary/all"
 
@@ -23,17 +64,17 @@ listData.reverse()
 matchData = []
 
 for i in listData:
-	time = i.find(class_ = 'time')
-	if str(time.contents) == "[]":
-		listData.remove(i)
-		continue
-	else:
-		data = i.find('p')
-		data = data.contents
-		highlight = i.attrs["class"]
-		
-		current = Events(time.contents[0], highlight[0], data[0])
-		matchData.append(current)
+    time = i.find(class_ = 'time')
+    if str(time.contents) == "[]":
+        listData.remove(i)
+        continue
+    else:
+        data = i.find('p')
+        data = data.contents
+        highlight = i.attrs["class"]
+        
+        current = Events(time.contents[0], highlight[0], data[0])
+        matchData.append(current)
 
 for i in matchData:
-	print i.t + " --> " + i.hl + "\n" + i.d + "\n\n"
+    print i.t + " --> " + i.hl + "\n" + i.d + "\n\n"
--- starting11.py
+++ starting11.py
@@ -5,50 +5,52 @@ import json
 
 # For future Use
 # class Player:
-# 	def __init__(self, team, player, rating, position):
-# 		self.t = team
-# 		self.p = player
-# 		self.r = rating
-# 		self.pos = position
+#     def __init__(self, team, player, rating, position):
+#         self.t = team
+#         self.p = player
+#         self.r = rating
+#         self.pos = position
 
 
 def getTeam(link):
-	response = urllib2.urlopen(link)
-	html_doc = response.read()
+    response = urllib2.urlopen(link)
+    html_doc = response.read()
 
-	soup = BeautifulSoup(html_doc)
+    soup = BeautifulSoup(html_doc)
 
-	listData = soup.find_all('ul', {'class' : 'sq-lst'})
+    listData = soup.find_all('ul', {'class' : 'sq-lst'})
 
-	hLineup = soup.find('a', {'href': '#teamlineup-homeTeam'})
-	aLineup = soup.find('a', {'href': '#teamlineup-awayTeam'})
-	home = str(hLineup.contents[0].strip())
-	away = str(aLineup.contents[0].strip())
-	teams[home] = []
-	teams[away] = []
+    hLineup = soup.find('a', {'href': '#teamlineup-homeTeam'})
+    aLineup = soup.find('a', {'href': '#teamlineup-awayTeam'})
+    home = str(hLineup.contents[0].strip())
+    away = str(aLineup.contents[0].strip())
+    teams[home] = []
+    teams[away] = []
 
-	listH = listData[1].find_all('li')
-	listHS = listData[2].find_all('li')
-	listH += listHS
+    listH = listData[1].find_all('li')
+    listHS = listData[2].find_all('li')
+    listH += listHS
 
-	for i in listH:
-		sp = i.find_all("span")
-		pl = sp[1].contents
-		teams[home].append(str(pl[0].strip()))
+    for i in listH:
+        sp = i.find_all("span")
+        pl = sp[1].contents
+        teams[home].append(str(pl[0].strip()))
 
-	listA = listData[4].find_all('li')
-	listAS = listData[5].find_all('li')
-	listA += listHS
+    listA = listData[4].find_all('li')
+    listAS = listData[5].find_all('li')
+    listA += listHS
 
-	for i in listA:
-		sp = i.find_all("span")
-		pl = sp[1].contents
-		teams[away].append(str(pl[0].strip()))	
+    for i in listA:
+        sp = i.find_all("span")
+        pl = sp[1].contents
+        teams[away].append(str(pl[0].strip()))
+            
 
 
 if __name__=="__main__":
-	link = 'http://www1.skysports.com/football/live/match/279816/teams'
-	teams = {}
-	getTeam(link)
-	with open('teams.json', 'w') as outfile:
-		json.dump(teams, outfile)
+    #link = 'http://www1.skysports.com/football/live/match/279816/teams'
+    link = 'http://www1.skysports.com/football/live/match/313388/teams'
+    teams = {}
+    getTeam(link)
+    with open('teams.json', 'w') as outfile:
+        json.dump(teams, outfile)
